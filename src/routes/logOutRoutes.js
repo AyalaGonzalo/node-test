a902cf8e87a4378eaad5d5d619606ee3ee8a6de3
Ayala Gonzalo <SoloEstudioTrabajo@gmail.com>
2023-12-09 21:54:04 -0300

    fix

--- src/routes/logOutRoutes.js
+++ src/routes/logOutRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 
-const getLogOut = require('../controller/auth/getLogOut.js')
+const getLogOut = require('../controller/auth/getLogout.js')
 
 router.get('/auth/logout', getLogOut)
 
